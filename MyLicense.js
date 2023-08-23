46a4b527255f35cef39447df425cecbc214277b7
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2023-08-23 12:17:27 -0700

    Update MyLicense.js

--- MyLicense.js
+++ MyLicense.js
@@ -1,7 +1,7 @@
 var licenseKey = require('license-key-gen');
 
 var userInfo = {company:"wendellmesettech",street:"2 W D", city:"Orange", state:"Cal", zip:"98888"}
-var licenseData = {info:userInfo, prodCode:"LEN100120", appVersion:"1.0", osType:'PINGU1'}
+var licenseData = {info:StudentUser, prodCode:"EEN850392", appVersion:"1.0", osType:'PINGU1'}
 
 try{
     var license = licenseKey.createLicense(licenseData)
